3f69a95a83b4130932374fd40406f0e368cbe131
Adam Renklint <adam@renklint.com>
2025-08-31 14:58:29 +0200

    Initial commit

---
+++ main.cljs
@@ -0,0 +1,23 @@
+(ns main
+  (:require
+    [reagent.core :as r]
+    [reagent.dom :as rdom]))
+
+(defonce !counter (r/atom 0))
+
+(defn increment! []
+  (swap! !counter inc)
+  ; (js/setTimeout increment! 100)
+  (js/window.requestAnimationFrame increment!))
+
+(defn app []
+  [:main
+   [:h1 "Outer counter"]
+   [:pre "Count: " (pr-str @!counter)]
+   [:iframe {:src "https://adamrenklint.com/outer-sandbox-test"
+             :style {:height 400 :width 500
+                     :border "1px solid lightgrey"}}]])
+
+(rdom/render [app] (.getElementById js/document "app"))
+
+(increment!)
